0118fc23265b9b1f7a33bb9ad8ca852fe540d191
Channabasava <channabasavabml@gmail.com>
2019-11-28 19:24:18 +0530

    [channabasava]refactor.made some changes

--- functionalPrograms/windChill/wind.js
+++ functionalPrograms/windChill/wind.js
@@ -7,6 +7,6 @@
  */
 const aceess = require('./windBl');
 let input = require("readline-sync");
-let temp = input.questionFloat("Enter the temperature (in Fahrenheit)");
-let speed = input.questionFloat("Enter the wind speed (Miles per hour) ");
+let temp = input.questionFloat("Enter the temperature is(in Fahrenheit)");
+let speed = input.questionFloat("Enter the wind speed is(Miles per hour) ");
 aceess.windchil(temp, speed);
