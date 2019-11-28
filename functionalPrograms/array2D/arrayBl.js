30576fa312885ff5ed706c32a01f4c2b13fbb5b9
Channabasava <channabasavabml@gmail.com>
2019-11-28 18:57:09 +0530

    [channabasava]refactor.made some changes progream

--- functionalPrograms/array2D/arrayBl.js
+++ functionalPrograms/array2D/arrayBl.js
@@ -9,7 +9,7 @@ exports.array2d = (row, col) => {
     for (let i = 0; i < row; i++) {
       array.push([]);
       for (let j = 0; j < col; j++) {
-        var array2 = input.question("enter the elements");
+        var array2 = input.question("enter the elements in the array");
         array[i][j] = array2;
       }
     }
