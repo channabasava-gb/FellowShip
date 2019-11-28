1fd5896f81bfc56167e961bf3cfecd2814af7b25
Channabasava <channabasavabml@gmail.com>
2019-11-28 19:18:00 +0530

    [channabasava]refactor.some changes program

--- functionalPrograms/stopWatch/stopBl.js
+++ functionalPrograms/stopWatch/stopBl.js
@@ -8,8 +8,8 @@ exports.stopwatch = (starttime, stopwatch, elapsedtime) => {
   console.log('the strat time is:' + starttime);
   var x = new Date();
   stoptime = x.getMilliseconds();
-  console.log('stop time is:' + stoptime);
+  console.log('stop time is the:' + stoptime);
   elapsedtime = stoptime - starttime;
-  console.log('lose time is:' + elapsedtime);
+  console.log('lose time is the:' + elapsedtime);
   return elapsedtime;
 };
