8d00a17b5afcc36d7e1cd010b8ae4ca9b9818c69
Channabasava <channabasavabml@gmail.com>
2019-12-02 15:41:03 +0530

    [channabasav]add.created oopsConcepts branch and added addressBook program

--- algorithmPrograms/primeNumbers/prime.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file: prime.js.
- * @module: algorithm
- * @author: channabasava
- * @version:6.9.0
- * @since:13-Nov-2019
- */
-const access = require('../../utility/utility');
-access.primeNumbers();
--- functionalPrograms/gambler/gambler.js
+++
@@ -1,6 +0,0 @@
-const get = require('./gamblerBl');
-let input = require("readline-sync");
-let stake = input.question("enter the stake ");
-let goal = input.question("enter the goal ");
-let trial = input.question("eumber of traials ");
-get.gambler(stake, goal, trial);
