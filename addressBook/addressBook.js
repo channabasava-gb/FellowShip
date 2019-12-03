d69c1131d4c819c4e66241b6457fe25b4855df89
Channabasava <channabasavabml@gmail.com>
2019-12-03 18:05:04 +0530

    [channabasava]refactor.modified addressBook program for better readability

--- addressBook/addressBook.js
+++ addressBook/addressBook.js
@@ -1,6 +1,15 @@
+/****************************************************************************************
+* @Execution : default node : cmd> addressBook.js
+* @description :to read the details of the person address
+* @overview : To print the person addrress
+* @author : channabasava <channabasavabml@gmail.com>
+* @version : 6.9.0
+* @since : 13 November 2019
+******************************************************************************************/
+
 try {
     const fs = require('fs')
-    const address_Book = require("../addressBook/adderssBookBl")
+    const address_Book = require("../addressBook/addressBookBl")
     let jsonFile = JSON.parse(fs.readFileSync("/home/admin1/channabasava/fellowshipJavascript/oops/addressBook/addressBook.json"))
     address_Book.addressOperations(jsonFile)
     } catch (e) {
