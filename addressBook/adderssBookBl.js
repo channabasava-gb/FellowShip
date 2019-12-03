60de4645435686450a8a5de8af43b1b78ae6d982
Channabasava <channabasavabml@gmail.com>
2019-12-03 17:36:26 +0530

    [channabasava]refactor.modified addressBook program and pushed into remot

--- addressBook/adderssBookBl.js
+++ addressBook/adderssBookBl.js
@@ -1,47 +1,91 @@
-//importing  the fs
-let fs = require('fs')
-//importing the readlinesync
-let r = require('readline-sync')
-//creating the addressbook class
-class AddressBook {
-    constructor() {
-        this.content = null;
-        this.fname = null;
+const input = require("readline-sync")
+const fs = require('fs')
+//main function that shows the options to select and takes the values
+addressOperations = (jsonFile) => {
+    let N = input.questionInt("press \n 1) Edit the person \n 2) Add a new person \n 3) Delete person \n 4) Search by name\n 5) Print all contacts \n")
+    switch (N) {
+        case 1:
+            editPerson(jsonFile);
+            break;
+        case 2:
+            addPerson(jsonFile);
+            break;
+        case 3:
+            deletePerson(jsonFile);
+            break;
+        case 4:
+            searchByName(jsonFile);
+            break;
+        case 5:
+            printFile(jsonFile);
+            break;
     }
 }
-AddressBook.prototype.jsonParse = (fname) => {
-    try {
-        this.fname = fname;
-        this.content = JSON.parse(fs.readFileSync(this.fname));
-        console.log("file name--->", this.fname);
-        console.log("Contye--->", this.content);
-
-
-    } catch (err) {
-        console.log("error" + err)
+//edits the details of the person and update them
+editPerson = (jsonFile) => {
+    let i = input.questionInt('Enter the index of the person to edit\n')
+    let content = input.questionInt(' 1) Edit firstName\n 2) Edit lastName\n 3) Edit houseNo\n 4) Edit taluk\n 5)Edit distric\n 6)Edit state\n')
+    console.log(jsonFile[i])
+    let update = input.questionInt('Enter the new content to update\n')
+    switch (content) {
+        case 1:
+            jsonFile[i].firstName = update;
+            break;
+        case 2:
+            jsonFile[i].lastName = update;
+            break;
+        case 3:
+            jsonFile[i].houseNo = update;
+            break;
+        case 4:
+            jsonFile[i].taluk = update;
+            break;
+        case 5:
+            jsonFile[i].distric = update;
+            break;
+        case 5:
+            jsonFile[i].state = update;
+            break;
     }
+    fs.writeFileSync('/home/admin1/channabasava/fellowshipJavascript/oops/addressBook/addressBook.json', JSON.stringify(jsonFile))
+    console.log("Updated successfully")
+    return jsonFile;
 }
+// adds a new person to the json file
+addPerson = (jsonFile) => {
+    let newPerson = {};
+    newPerson.firstName = input.question("Enter the firstName :")
+    newPerson.lastName = input.question("Enter the lastName:")
+    newPerson.houseNo = input.question("Enter the houseNo :")
+    newPerson.taluk = input.question("Enter the taluk :")
+    newPerson.distric = input.question("Enter the distric :")
+    newPerson.state = input.question("Enter the state :")
 
-//it will add address to the json file
-AddressBook.prototype.addAddress = (firstname, lastname, houseno = null, city = null, district = null) => {
-
-    this.content.push({
-        "firstname": firstname,
-        "lastname": lastname,
-        "houseno": houseno,
-        "city": city,
-        "district": district
-
-    })
-    console.log("added succefully");
-    console.log(this.content);
-    fs.writeFileSync(this.fname, JSON.stringify(this.content));
-
+    jsonFile.push(newPerson)
+    fs.writeFileSync("/home/admin1/channabasava/fellowshipJavascript/oops/addressBook/addressBook.json", JSON.stringify(jsonFile))
+    console.log("ADDED SUCCESSFULLY...")
+    return jsonFile;
 }
-AddressBook.prototype.jsonSave = () => {
-
-
-
+// deletes a person from the json file
+deletePerson = (jsonFile) => {
+    let i = input.question("Enter the index number to delete the contact:")
+    jsonFile.pop(i)
+    fs.writeFileSync("/home/admin1/channabasava/fellowshipJavascript/oops/addressBook/addressBook.json", JSON.stringify(jsonFile))
+    console.log("Deleted successfully....")
+    return jsonFile
 }
-//export the class
-module.exports = AddressBook;
+// searchs a person by the name
+searchByName = (jsonFile) => {
+    let Name = input.question("Enter the firstName or lastName:")
+    jsonFile.forEach(nameInRecord => {
+        if (nameInRecord.firstName.includes(Name) || nameInRecord.lastName.includes(Name))
+            console.log(nameInRecord)
+    });
+}
+//prints the person in json file
+printFile = (jsonFile) => {
+    jsonFile.forEach(person => {
+        console.log(person)
+    });
+}
+module.exports = { addressOperations }
--- addressBook/addressBook.js
+++ addressBook/addressBook.js
@@ -1,22 +1,8 @@
-let AddressBook = require('../addressBook/adderssBookBl')
-//creating a object of the addressbook
-let addressObject = new AddressBook();
-//parsing json into json object
-addressObject.jsonParse('/home/admin1/channabasava/fellowshipJavascript/oops/addressBook/addressBook.json');
-//adding address to the addressbook
-
-const read = require('readline-sync');
-
-var choice = read.question("Enter you choice\n 1: Add Record\n");
-switch (parseInt(choice)) {
-    case 1: let fname = read.question("Enter the first fname\n");
-        let lname = read.question("Enter the first lname\n");
-        let hno = read.question("Enter the first house no\n");
-        let place = read.question("Enter the first place\n");
-        let dis = read.question("Enter the first district\n");
-        addressObject.addAddress(fname, lname, hno, place, dis);
-        break;
-    
-
-}
-
+try {
+    const fs = require('fs')
+    const address_Book = require("../addressBook/adderssBookBl")
+    let jsonFile = JSON.parse(fs.readFileSync("/home/admin1/channabasava/fellowshipJavascript/oops/addressBook/addressBook.json"))
+    address_Book.addressOperations(jsonFile)
+    } catch (e) {
+    console.log(e.message)
+    }
