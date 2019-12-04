147ab8c6c920a77a357b01260b7176b690986348
Channabasava <channabasavabml@gmail.com>
2019-12-04 16:31:39 +0530

    [channabasava]refactor.modified protoypeDesign pattern for better readability

--- algorithmPrograms/anagram/angram.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file: Anagram.js.
- * @module: Algorithm
- * @author: Channabasava
- * @since:13-Nov-2019
- */
-const access = require('../../utility/utility');
-access.anagram();
--- algorithmPrograms/mergeSort/mergesort.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file: mergesort.js.
- * @module: algorithm
- * @author: channabasava
- * @version: 
- * @since:13-Nov-2019
- */
-const access = require('../../utility/utility');
-access.mergeSort();
